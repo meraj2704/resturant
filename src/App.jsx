b98c7c524cfed5fa2de0a844c9f1bafb8e3b7f60
merajHossain <mh1669101@gmail.com>
2024-01-03 12:20:22 +0600

    restaurant navbar section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
-import Header from './Container/Header/Header'
-import AboutUs from './Container/AboutUs/AboutUs'
-import SpecialMenu from './Container/Menu/SpecialMenu'
-import Chef from './Container/Chef/Chef'
-import Intro from './Container/AboutUs/Intro/Intro'
-import Laurels from './Container/Laurels/Laurels'
-import Gallery from './Container/Gallery/Gallery'
-import FindUs from './Container/Findus/FindUs'
-import Footer from './Container/Footer/Footer'
-
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Header from "./Container/Header/Header";
+import AboutUs from "./Container/AboutUs/AboutUs";
+import SpecialMenu from "./Container/Menu/SpecialMenu";
+import Chef from "./Container/Chef/Chef";
+import Intro from "./Container/AboutUs/Intro/Intro";
+import Laurels from "./Container/Laurels/Laurels";
+import Gallery from "./Container/Gallery/Gallery";
+import FindUs from "./Container/Findus/FindUs";
+import Footer from "./Container/Footer/Footer";
 
 function App() {
   return (
-   <div>
-    <Navbar></Navbar>
-   <Header></Header>
-    <AboutUs></AboutUs>
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
-   </div>
-  )
+    <div>
+      <Navbar></Navbar>
+      <Header></Header>
+      <AboutUs></AboutUs>
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
