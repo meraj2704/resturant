2c5c30eb8ae121ace9665144657bc01c034dc1ff
merajHossain <mh1669101@gmail.com>
2024-01-04 08:29:44 +0600

    restaurent header section done

--- src/Container/AboutUs/AboutUs.jsx
+++ src/Container/AboutUs/AboutUs.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src="https://i.ibb.co/sPkZrZ3/G.png" alt="" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img
+          src="https://i.ibb.co/j4YWL0X/spoon.png"
+          className="spoon__img"
+          alt=""
+        />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife">
+        <img src="https://i.ibb.co/Bch5pKY/knife.png" alt="" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img
+          src="https://i.ibb.co/j4YWL0X/spoon.png"
+          className="spoon__img"
+          alt=""
+        />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+
+    </div>
   </div>
 );
 
