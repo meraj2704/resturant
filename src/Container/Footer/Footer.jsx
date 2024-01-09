582c276c5529174a840a8ee502e408d32fa7bb01
merajHossain <mh1669101@gmail.com>
2024-01-09 12:09:58 +0600

    newsletter

--- src/Container/Footer/Footer.jsx
+++ src/Container/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import {FiFacebook,FiTwitter,FiInstagram} from 'react-icons/fi'
 import './Footer.css';
+import FooterOverlay from '../../Components/Footer/FooterOverlay';
+import Newsletter from '../../Components/Footer/Newsletter';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <FooterOverlay></FooterOverlay>
+    <Newsletter></Newsletter>
   </div>
 );
 
